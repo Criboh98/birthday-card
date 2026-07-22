07f1a29550598ad4ac596605d81da7da4444cefc
Criboh98 <murcianocristiana@gmail.com>
2026-07-22 14:04:00 +0200

    Add birthday message and improve typewriter effect
    Added a birthday message and enhanced the typewriter effect for displaying it.

--- script.js
+++ script.js
@@ -1,7 +1,108 @@
+const message = `
+Grattis på födelsedagen! ❤️
+
+Det här kortet kommer lite i förväg eftersom jag reser bort och inte ville riskera att missa din dag.
+
+Jag hoppas att den lilla presenten kan göra dig glad och hjälpa dig att fortsätta “bygga” din One Piece.
+
+Jag kommer alltid att heja på dig och hoppas att du hittar det du söker.
+
+Och du vet ju… du är il mio cielo in una stanza. 💙
+
+Ta hand om dig, och hoppas att du får en riktigt fin födelsedag.
+`;
+
+
+
 function openTreasure(){
 
-document.querySelector(".container").style.display="none";
 
-document.getElementById("letter").style.display="block";
+    // hide intro
+
+    document.getElementById("intro")
+    .classList.remove("active");
+
+
+
+    // show treasure
+
+    document.getElementById("treasure")
+    .classList.add("active");
+
+
+
+    setTimeout(()=>{
+
+
+        document.getElementById("treasure")
+        .classList.remove("active");
+
+
+        document.getElementById("letterScene")
+        .classList.add("active");
+
+
+        typeWriter();
+
+
+    },3000);
+
+
+
+}
+
+
+
+
+let index = 0;
+
+
+function typeWriter(){
+
+
+    let text =
+    document.getElementById("letterText");
+
+
+    if(index < message.length){
+
+
+        text.innerHTML += message.charAt(index);
+
+
+        index++;
+
+
+        setTimeout(typeWriter,35);
+
+
+    }
+
+
+    else{
+
+
+        setTimeout(showEnding,4000);
+
+
+    }
+
+
+}
+
+
+
+
+
+function showEnding(){
+
+
+    document.getElementById("letterScene")
+    .classList.remove("active");
+
+
+    document.getElementById("ending")
+    .classList.add("active");
+
 
 }
