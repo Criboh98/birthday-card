59f7d2564239a839567d8000a37cbae0493c1b68
Criboh98 <murcianocristiana@gmail.com>
2026-07-22 13:44:54 +0200

    Add functionality to open letter on button click

---
+++ script.js
@@ -0,0 +1,11 @@
+const button=document.getElementById("openButton");
+
+const letter=document.getElementById("letter");
+
+button.onclick=function(){
+
+button.style.display="none";
+
+letter.style.display="block";
+
+}
